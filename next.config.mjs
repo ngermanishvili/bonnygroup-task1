f79d03fef22f22be37bbeacbaedd7fff71c1485c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-04-08 23:18:43 +0400

    wasm upd

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,12 @@ export default {
         },
     },
     basePath: '',
+    restrictMode: false, // Add this line to disable restrict mode
+
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         // Note: we provide webpack above so you should not `require` it
         // Perform customizations to webpack config
+
         const destWasmFolder = "."
         config.plugins.push(new CopyPlugin({
             patterns: [
